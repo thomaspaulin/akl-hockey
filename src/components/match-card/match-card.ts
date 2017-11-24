7e5ea4f2923969b81bb86f5c78bf3cbf5f584677
Thomas P <thomas@paulin.nz>
2017-11-24 20:13:38 +1300

    Update match card to work with new API format

--- src/components/match-card/match-card.ts
+++ src/components/match-card/match-card.ts
@@ -1,7 +1,6 @@
 import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Team} from '../../model/Team';
 import {Match} from '../../model/Match';
-import {UNKNOWN_TEAM} from "../../app/app.constants";
 
 @Component({
   selector:        'match-card',
@@ -13,25 +12,10 @@ export class MatchCardComponent implements OnInit {
   @Input() teams: Array<Team>;
   @Output() cardTapped = new EventEmitter<Match>();
 
-  away: Team;
-  home: Team;
-
   constructor() {
   }
 
   ngOnInit(): void {
-    this.away = UNKNOWN_TEAM;
-    this.home = UNKNOWN_TEAM;
-    if (this.teams) {
-      let retrievedAway = this.teams.find(team => this.away.name.toLowerCase() === team.name.toLowerCase());
-      let retrievedHome = this.teams.find(team => this.home.name.toLowerCase() === team.name.toLowerCase());
-      if (retrievedAway) {
-        this.away = retrievedAway;
-      }
-      if (retrievedHome) {
-        this.home = retrievedHome;
-      }
-    }
   }
 
   onCardTapped(m: Match) {
