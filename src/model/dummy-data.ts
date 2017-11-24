cb10135764aac1ff60e2cd9415fbb662af528c7b
Thomas P <thomas@paulin.nz>
2017-11-24 20:10:54 +1300

    Redo dummy data in new API format

--- src/model/dummy-data.ts
+++ src/model/dummy-data.ts
@@ -1,63 +1,81 @@
-import { Team } from './Team';
-import { Match } from './Match';
+import {Team} from './Team';
+import {Match} from './Match';
+import {Rink} from "./Rink";
+import {Division} from "./Division";
 
 export namespace db {
-  const _teams = {
+  const _divisions: { [key: string]: Division } = {
+    'c': {ID: 1, name: 'C'},
+    'b': {ID: 2, name: 'B'},
+    'a': {ID: 3, name: 'A'},
+  };
+
+  const _rinks: { [key: string]: Rink } = {
+    'avondale': {ID: 1, name: 'Avondale'},
+    'botany': {ID: 1, name: 'Botany'},
+  };
+
+  const _teams: { [key: string]: Team } = {
     'bears': {
         name:     'Bears',
-        division: 'C',
+      division: _divisions['c'],
         logoURL:  'http://placekitten.com/g/40/40'
     },
     'hawks': {
         name:     'Hawks',
-        division: 'C',
+      division: _divisions['c'],
         logoURL:  'http://placekitten.com/g/40/40'
     },
     'lions': {
       name: 'Lions',
-      division: 'C',
+      division: _divisions['c'],
       logoURL: 'http://placekitten.com/g/40/40'
     },
     'tigers': {
       name: 'Tigers',
-      division: 'C',
+      division: _divisions['c'],
       logoURL: 'http://placekitten.com/g/40/40'
     },
     'scorpions': {
       name: 'Scorpions',
-      division: 'C',
+      division: _divisions['c'],
       logoURL: 'http://placekitten.com/g/40/40'
     }
   };
 
   export const teams: Array<Team> = Object.keys(_teams).map(key => _teams[key]);
-
+  export const rinks: Array<Rink> = Object.keys(_rinks).map(key => _rinks[key]);
+  export const divisions: Array<Rink> = Object.keys(_divisions).map(key => _divisions[key]);
   export const matches: Array<Match> = [
     {
+      ID: 1,
       date: new Date(),
-      rink: 'Botany',
-      away: 'bears',
+      rink: _rinks['botany'],
+      away: _teams['bears'],
       awayScore: 5,
-      home: 'hawks',
+      home: _teams['hawks'],
       homeScore: 2
     },
     {
+      ID: 2,
       date: new Date(),
-      rink: 'Avondale',
-      away: 'hawks',
-      home: 'bears'
+      rink: _rinks['avondale'],
+      away: _teams['hawks'],
+      home: _teams['bears']
     },
     {
+      ID: 3,
       date: new Date(),
-      rink: 'Avondale',
-      away: 'hawks',
-      home: 'tigers'
+      rink: _rinks['avondale'],
+      away: _teams['hawks'],
+      home: _teams['tigers']
     },
     {
+      ID: 4,
       date: new Date(),
-      rink: 'Botany',
-      away: 'lions',
-      home: 'scorpions'
+      rink: _rinks['botany'],
+      away: _teams['lions'],
+      home: _teams['scorpions']
     }
   ];
 }
